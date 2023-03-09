7bce6e9b5cb0824774b1aeec37d513870e2f167f
naderkhani <m.naderkhani@kavano.co>
2023-03-09 19:27:17 +0330

    object type

--- app.ts
+++ app.ts
@@ -1,1 +1,18 @@
-console.log("Hello world");
+// Object type
+// Good ✔
+const person = {
+  name: "John",
+  age: 30,
+};
+
+// Bad ❌
+const personCopy: object = {
+  name: "John",
+  age: 30,
+};
+
+// Bad ❌
+const personCopy2: { name: string; age: number } = {
+  name: "John",
+  age: 30,
+};
